617e2846f8ef5f77c554f6900a22a0438336f47f
z4Beemer <adrianramonfernandez01@gmail.com>
2018-10-03 02:10:11 -0400

    Updated project3 - added javascript - js file in js folder - used tables

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+/* Below are functions in javascript that will be used in multiple pages*/
+
+function bigImg(x) {
+    x.style.height = "600px";
+    x.style.width = "800px";
+}
+
+function normalImg(x) {
+    x.style.height = "400px";
+    x.style.width = "600px";
+}
+
+function buttonImg(x){
+    x.style.height = "300px";
+    x.style.width = "300px";
+}
+
+function buttonImg2(x) {
+    x.style.height = "76px";
+    x.style.width = "76px";
+}
